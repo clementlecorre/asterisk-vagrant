8ddab2355833cb335985f81086e4dfa2d0110663
Ceylass <maxime.ninetyfive@gmail.com>
2017-01-31 12:19:25 +0100

    Update 02-configure.sh

--- libs/02-configure.sh
+++ libs/02-configure.sh
@@ -7,6 +7,7 @@ cp $DEFAULT_DIR/users.conf /etc/asterisk/users.conf
 cp $DEFAULT_DIR/voicemail.conf /etc/asterisk/voicemail.conf
 cp $DEFAULT_DIR/extensions.conf /etc/asterisk/extensions.conf
 cp $DEFAULT_DIR/sip.conf /etc/asterisk/sip.conf
+cp $DEFAULT_DIR/confbridge.conf /etc/asterisk/confbridge.conf
 
 # asterisk restart
 /etc/init.d/asterisk restart
